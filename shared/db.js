4f8c80ee6957de547c51babd14a559a97ffb0f52
ApokaliseGames <apokalisegames@gmail.com>
2019-02-05 18:19:39 -0200

    db local and live

--- shared/db.js
+++ shared/db.js
@@ -1,6 +1,6 @@
 const config = require('../config.json');
 const mongoose = require('mongoose');
-mongoose.connect(config.connectionString);
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useNewUrlParser: true, useCreateIndex: true });
 mongoose.Promise = global.Promise;
 module.exports = {
     User: require('../features/user/user.model'),
@@ -9,4 +9,3 @@ module.exports = {
     Income: require('../features/income/income.model'),
     Credit: require('../features/credit/credit.model')
 };
-
